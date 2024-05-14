98e1e791c524e01d25a0beff3a882304e3d73563
rodrigo <rodrigo.martinez224@gmail.com>
2024-05-14 05:57:42 -0300

    v2

---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,24 @@
+import { Type } from "class-transformer";
+import { IsNumber, IsString, Min } from "class-validator";
+
+
+
+
+export class CreateProductDto {
+
+
+
+    @IsString()
+    public name: string;
+
+
+    @IsNumber({
+        maxDecimalPlaces: 4,
+    })
+    @Min(0)
+    @Type(() => Number) // esto transforma el string a numero si puede 
+    public price: string;
+
+
+
+}
---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,16 @@
+
+
+
+
+export class Product {
+
+    public id: number; // 12345 en la vida real deberia usar uuid
+
+    public name: string;
+
+    public price: number;
+
+
+
+
+}
