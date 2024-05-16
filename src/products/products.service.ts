465e0ab9c4f8f8a1c813f635a2e16734d986a468
rodrigo <rodrigo.martinez224@gmail.com>
2024-05-16 08:35:21 -0300

    v6

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,7 +1,8 @@
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
+import { PaginationDto } from 'src/common';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
@@ -23,21 +24,79 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
 
   }
 
-  findAll() {
+  // esto de abajo permite buscar todo de la base de datos con la pagination incluido 
+  async findAll(paginationDto: PaginationDto) {
 
-    return this.product.findMany({});
+    const { page, limit } = paginationDto
+
+    const totalPages = await this.product.count({ where: { available: true } });
+
+    const lastPage = Math.ceil(totalPages / limit);
+
+    return {
+      data: await this.product.findMany({
+        skip: (page - 1) * limit,
+        take: limit,
+        where: {
+          available: true
+        }
+      }),
+      meta: {
+        total: totalPages,
+        page: page,
+        lastPage: lastPage,
+      }
+    }
 
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  // esto de abajo permite buscar por id 
+  async findOne(id: number) {
+    const product = await this.product.findFirst({
+      where: { id, available: true }
+    })
+
+    if (!product) {
+      throw new NotFoundException(`Product with id #${id} not found`);
+    }
+    return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+
+
+
+
+  async update(id: number, updateProductDto: UpdateProductDto) {
+
+    await this.findOne(id);
+
+
+    return this.product.update({
+      where: { id },
+      data: updateProductDto,
+    });
+
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+
+
+
+  async remove(id: number) {
+
+    await this.findOne(id);
+
+    // return this.product.delete({
+    //   where: { id } });
+
+    // esto de abajo es soft delete envia lo que quiero borrar a que no este available  el otro de arriba solo lo elimina de la base de datos
+    const product = await this.product.update({
+      where: { id },
+      data: {
+        available: false
+      }
+    })
+
+    return product;
+
   }
 }
